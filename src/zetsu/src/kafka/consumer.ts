3d056ae8810721b7c96d2e3b6186555988406f76
Arjav Dave <arjav@royalecheese.com>
2023-06-13 23:20:27 +0530

    Inserting User from consumer

--- src/zetsu/src/kafka/consumer.ts
+++ src/zetsu/src/kafka/consumer.ts
@@ -1,10 +1,13 @@
 import { logger } from "@cashoco/common";
 import { Kafka } from "kafkajs";
 import { TOPIC_HEALTH } from "./topics";
+import { User } from "../models/user";
+import connectDB from "../database";
 
-const KAFKA_BROKER:string = process.env.KAFKA_BROKER!;
- 
+const KAFKA_BROKER: string = process.env.KAFKA_BROKER!;
+
 async function processMessage() {
+    await connectDB()
     const kafka = new Kafka({
         brokers: [KAFKA_BROKER]
     })
@@ -12,23 +15,33 @@ async function processMessage() {
         groupId: 'health_consumer_group_node'
     })
     await consumer.connect()
-        .then(()=>{
+        .then(() => {
             logger.info('Connected to kafka server');
         })
-        .catch(error=>{
+        .catch(error => {
             logger.info(`error encountered while consumer connect ${error.message}`);
-        }); 
+        });
     await consumer.subscribe({ topics: [TOPIC_HEALTH], fromBeginning: true })
-        .catch(error=>{
+        .catch(error => {
             logger.info(`error encountered while consumer subscribing ${error.message}`)
         })
     await consumer.run({
         eachBatchAutoResolve: true,
         eachMessage: async ({ message }: any) => {
+            User.create({
+                email: 'test@casho.com',
+                name: 'Test Name'
+
+            }).then(res => {
+                console.log(`RESPONSE ${res}`);
+            }).catch(err => {
+                console.log(`ERROR: ${err}`);
+            })
+
             logger.info(`Recieved Message on Node :${message.value?.toString()}`);
-            },
+        },
     })
-} 
-processMessage().catch(error=>{
+}
+processMessage().catch(error => {
     logger.info(error);
-    })
+})
