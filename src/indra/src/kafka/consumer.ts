729fed9e54ff5d842de0a8e3b5ec75b8ad76829a
Yash-Makwana1 <yash.m@royalecheese.com>
2023-06-13 13:47:30 +0530

    Task-2/Sample of kafka in indra

--- src/indra/src/kafka/consumer.ts
+++ src/indra/src/kafka/consumer.ts
@@ -1,13 +1,10 @@
 import { logger } from "@cashoco/common";
 import { Kafka, Consumer } from "kafkajs";
 import { Topic_health } from "./topics";
+import { error, log } from "console";
 
 export class KafkaConsumer {
-
-    constructor() {
-
-    }
-
+    
     public async Process_message() {
         const kafka = new Kafka({
             brokers: ['kafka-service:9092']
@@ -15,9 +12,17 @@ export class KafkaConsumer {
         const consumer = kafka.consumer({
             groupId: 'health_consumer_group_node'
         })
-        await consumer.connect();
-        await consumer.subscribe({ topics: [Topic_health], fromBeginning: true })
+        await consumer.connect()
+                        .then(()=>{
+                            logger.info('Connected to kafka server')})
+                        .catch(error=>{
+                            logger.info(`error encountered while consumer connect ${error.message}`)
+                        }); 
+        await consumer.subscribe({ topics: [Topic_health], fromBeginning: true }).catch(error=>{
+            logger.info(`error encountered while consumer subscribing ${error.message}`)
+        })
         await consumer.run({
+            eachBatchAutoResolve: true,
             eachMessage: async ({ message }: any) => {
                 logger.info(`Recieved Message on Node :${message.value?.toString()}`);
             },
