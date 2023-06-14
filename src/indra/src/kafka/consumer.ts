3020c4da923b0604b41316b0f25a23ac69dbc5c4
Yash-Makwana1 <yash.m@royalecheese.com>
2023-06-14 12:08:33 +0530

    indentation

--- src/indra/src/kafka/consumer.ts
+++ src/indra/src/kafka/consumer.ts
@@ -1,35 +1,35 @@
-import { logger } from "@cashoco/common";
-import { Kafka } from "kafkajs";
-import { TOPIC_HEALTH } from "./topics";
-import { error } from "console";
+import { logger } from '@cashoco/common';
+import { Kafka } from 'kafkajs';
+import { TOPIC_HEALTH } from './topics';
+import { error } from 'console';
 
-const KAFKA_BROKER:string = process.env.KAFKA_BROKER!;
- 
+const KAFKA_BROKER: string = process.env.KAFKA_BROKER!;
+
 async function processMessage() {
-    const kafka = new Kafka({
-        brokers: [KAFKA_BROKER]
+  const kafka = new Kafka({
+    brokers: [KAFKA_BROKER],
+  });
+  const consumer = kafka.consumer({
+    groupId: 'health_consumer_group_node',
+  });
+  await consumer
+    .connect()
+    .then(() => {
+      logger.info('Connected to kafka server');
     })
-    const consumer = kafka.consumer({
-        groupId: 'health_consumer_group_node'
-    })
-    await consumer.connect()
-        .then(()=>{
-            logger.info('Connected to kafka server');
-        })
-        .catch(error=>{
-            logger.info(`error encountered while consumer connect ${error.message}`);
-        }); 
-    await consumer.subscribe({ topics: [TOPIC_HEALTH], fromBeginning: true })
-        .catch(error=>{
-            logger.info(`error encountered while consumer subscribing ${error.message}`)
-        })
-    await consumer.run({
-        eachBatchAutoResolve: true,
-        eachMessage: async ({ message }: any) => {
-            logger.info(`Recieved Message on Node :${message.value?.toString()}`);
-            },
-    })
-} 
-processMessage().catch(error=>{
-    logger.info(error);
-    })
+    .catch((error) => {
+      logger.info(`error encountered while consumer connect ${error.message}`);
+    });
+  await consumer.subscribe({ topics: [TOPIC_HEALTH], fromBeginning: true }).catch((error) => {
+    logger.info(`error encountered while consumer subscribing ${error.message}`);
+  });
+  await consumer.run({
+    eachBatchAutoResolve: true,
+    eachMessage: async ({ message }: any) => {
+      logger.info(`Recieved Message on Node :${message.value?.toString()}`);
+    },
+  });
+}
+processMessage().catch((error) => {
+  logger.info(error);
+});
