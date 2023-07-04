3f0c9b733aac3e425e0285a2687e1b4fedae8c40
Yash-Makwana1 <yash.m@royalecheese.com>
2023-07-04 12:39:21 +0530

    Authentication completed

--- src/ashura/authy/urls.py
+++ src/ashura/authy/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("token/", views.create_token.as_view(), name="create_token"),
-    path("refresh/", views.refresh_token.as_view(), name='token_refresh'),
-    path("test/", views.token_test.as_view(), name="index"),
+    path("token/", views.CreateToken.as_view(), name="create_token"),
+    path("refresh/", views.TokenRefresh.as_view(), name='token_refresh'),
+    path("test/", views.TokenTest.as_view(), name="index"),
 ]
--- src/ashura/authy/views.py
+++ src/ashura/authy/views.py
@@ -2,17 +2,17 @@ import json
 import logging
 import time
 from django.http import JsonResponse
-from django.views import View
 from .middleware import TokenAuthenticationMiddleware
 from django.utils.decorators import method_decorator
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework.views import APIView
 
 logger = logging.getLogger('ashura_app')
 
-class create_token(View):
+class CreateToken(APIView):
     def post(self, request):
-        data = json.loads(request.body)
+        data = {"test":"test"}
         token = RefreshToken() 
         token['data'] = data  
         access = str(token.access_token)
@@ -20,12 +20,12 @@ class create_token(View):
         return JsonResponse({'access':access,'refresh':refresh})
 
 @method_decorator(TokenAuthenticationMiddleware , name='dispatch')        
-class token_test(View):
+class TokenTest(APIView):
     def post(self,request):
         logger.info(request.user)
         return JsonResponse({'message':'Token Verified'})
     
-class refresh_token(TokenRefreshView):
+class TokenRefresh(TokenRefreshView):
     def post(self,request):
         time.sleep(5)
         return super().post(request)
