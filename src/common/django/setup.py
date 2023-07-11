2f9a63bdaba346a241c7a16ff9d87c5e36eb82ca
Jaykishan <uchiha.jay007@gmail.com>
2023-07-11 22:09:59 +0530

    changed to shared module and gitignore for it.

--- src/common/django/setup.py
+++ src/common/django/setup.py
@@ -1,15 +1,4 @@
-import os
 from setuptools import setup, find_packages
-
-package = "src"
-
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
 setup(
     name='shared',
@@ -18,6 +7,7 @@ setup(
     author='Jay ',
     author_email='jaykishan@playcent.com',
     packages=find_packages(),
+    python_requires='>=3.9',
     install_requires=[
         'Django>=4.1',
         'confluent-kafka>=2.1.1'
